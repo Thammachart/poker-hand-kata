00b8b08745163257224cc6efabe4ea7c9cfb7c63
Thammachart <bustheheroall@gmail.com>
2017-07-16 17:02:15 +0700

    Highest Card Hand complete /w tests

--- src/test/java/org/GameEngineTest.java
+++ src/test/java/org/GameEngineTest.java
@@ -1,3 +1,10 @@
+/*
+    Issues:
+        Conviction
+        Observe your own Body Language, and colleage
+        Calm yourself in the middle of process
+*/
+
 package org;
 
 import org.junit.Test;
@@ -5,6 +12,7 @@ import org.junit.Ignore;
 import org.junit.Assert;
 
 import java.util.stream.*;
+import java.util.Arrays;
 
 import org.PokerHandTest.PokerHand;
 import org.CardTest.Card;
@@ -41,7 +49,6 @@ public class GameEngineTest {
     }
 
     @Test
-    @Ignore
     public void testPlayerAWinWithSecondHighestCard() {
         PokerHand handA = PokerHand.parse("C2", "H3", "S5", "C8", "HA");
         PokerHand handB = PokerHand.parse("C2", "H3", "S4", "C8", "HA");
@@ -50,21 +57,36 @@ public class GameEngineTest {
         Assert.assertEquals("A wins", result);
     }
 
+    @Test
+    public void testPlayerAWinWithLastHighestCard() {
+        PokerHand handA = PokerHand.parse("H3", "C5", "H6", "S7", "C8");
+        PokerHand handB = PokerHand.parse("H4", "C5", "H6", "S7", "C8");
+
+        String result = gameEngine.duel(handA, handB);
+        Assert.assertEquals("B wins", result);
+    }
+
     public static class GameEngine {
         public String duel(PokerHand handA, PokerHand handB) {
-            Card[] cardsA = handA.getCards();
-            Card[] cardsB = handB.getCards();
+            return highestCardResult(handA.getCards(), handB.getCards());
+        }
 
-            Card maxValueA = highestCard(handA.getCards());
-            Card maxValueB = highestCard(handB.getCards());
+        private String highestCardResult(final Card[] _cardsA, final Card[] _cardsB) {
+            Card[] cardsA = sort(_cardsA);
+            Card[] cardsB = sort(_cardsB);
 
-            int result = maxValueA.compareTo(maxValueB);
-            if(result > 0)
-                return "A wins";
-            else if(result < 0)
-                return "B wins";
-            
+            for(int i = cardsA.length - 1; i >= 0; i--) {
+                int result = cardsA[i].compareTo(cardsB[i]);
+                if(result > 0)
+                    return "A wins";
+                else if(result < 0)
+                    return "B wins";
+            }
             return "Tie";
+        }
+
+        protected Card[] sort(Card[] cards) {
+            return Stream.of(cards).sorted().toArray(Card[]::new);
         }
 
         protected Card highestCard(Card[] cards) {
